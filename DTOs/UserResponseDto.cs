3e79afc279ffcb2249628ead1ba74d915ab6c689
Marcelo Gennari <101405635+marcelocgf@users.noreply.github.com>
2025-05-16 23:36:39 -0300

    UserResponseDTO adjusted

--- DTOs/UserResponseDto.cs
+++ DTOs/UserResponseDto.cs
@@ -2,9 +2,9 @@ namespace NextQuest.DTOs
 {
     public class UserResponseDto
     {
+        public int Id { get; set; }
         public string Email { get; set; } = string.Empty;
         public string Username { get; set; } = string.Empty;
-        public string PasswordHash { get; set; } = string.Empty;
         public bool IsAdmin { get; set; }
     }
 }
