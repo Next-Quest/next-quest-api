001cd7bd9088cdd02d57c621be77a7db05845c3b
Marcelo Gennari <101405635+marcelocgf@users.noreply.github.com>
2025-05-16 23:04:38 -0300

    Program fix

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.EntityFrameworkCore;
 using NextQuest.Data;
 using BCrypt.Net;
+using NextQuest.Interfaces;
+using NextQuest.Services;
 using Swashbuckle.AspNetCore.Swagger;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,6 +11,7 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 
+builder.Services.AddScoped<IUserInterface, UserService>();
 builder.Services.AddControllers();
 builder.Services.AddSwaggerGen(c =>
     {
