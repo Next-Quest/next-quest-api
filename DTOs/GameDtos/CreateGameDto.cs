8a7ec09bc9a3c2f4c41fb49964a7f03ec9264233
Marcelo Gennari <101405635+marcelocgf@users.noreply.github.com>
2025-05-22 01:09:53 -0300

    Dto fix

--- DTOs/GameDtos/CreateGameDto.cs
+++ DTOs/GameDtos/CreateGameDto.cs
@@ -2,5 +2,8 @@ namespace NextQuest.DTOs.GameDtos;
 
 public class CreateGameDto
 {
-    
+    public string Title { get; set; } = "";
+    public int PublisherId { get; set; }
+    public int DeveloperId { get; set; }
+    public DateTime ReleaseDate { get; set; }
 }
