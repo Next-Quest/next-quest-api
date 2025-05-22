0c0597cc5a953bd21ff1c5971e6deadc00b23544
Marcelo Gennari <101405635+marcelocgf@users.noreply.github.com>
2025-05-22 00:49:32 -0300

    Game Service implementation

--- DTOs/GameDtos/UpdateGameDto.cs
+++ DTOs/GameDtos/UpdateGameDto.cs
@@ -2,5 +2,9 @@ namespace NextQuest.DTOs.GameDtos;
 
 public class UpdateGameDto
 {
-    
+    public int Id { get; set; }
+    public string? Title { get; set; }
+    public int? PublisherId { get; set; }
+    public int? DeveloperId { get; set; }
+    public DateTime? ReleaseDate { get; set; }
 }
